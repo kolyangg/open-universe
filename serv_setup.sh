ebccc6c8d1f756a37b4ef4ab6ae913e35b3dab29
Nikolay <nasilaev@edu.hse.ru>
2025-04-25 23:53:58 +0100

    upd

---
+++ serv_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+echo "Downloading main repo"
+git clone https://github.com/kolyangg/speech_enh.git
+
+echo "Downloading _miipher repo"
+mkdir _miipher
+cd _miipher
+git clone https://github.com/ajaybati/miipher2.0.git
+cd ..
+
+echo "Setting up environment..."
+models/universe/setup_simple.sh
+
+echo "Downloading data..."
+python3 utils/dataset_download.py
+
+echo "Ready to go"
---
+++ setup_simple.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+cd ~/speech_enh
+
+echo "Create universe env"
+source activate base
+conda create -n universe python=3.11.11
+
+echo "universe env created - installing torch and pre-requirements"
+pip install torch==2.5.1 torchvision torchaudio==2.5.1
+
+sudo apt update
+sudo apt install -y nvidia-cuda-toolkit
+
+conda install -y -c conda-forge gmpy2 numexpr
+
+
+#### Install requirements
+echo "installing requirements.txt"
+pip install -r models/universe/univ_requirements.txt
+
+
+### Other installations (separate)
+echo "installing other libraries"
+python -m pip install --no-deps -e models/universe 
+pip install onnxruntime
+pip install textgrid
+
+### Install unzip
+echo "unzip"
+sudo apt update
+sudo apt install unzip
