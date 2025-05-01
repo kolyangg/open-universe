faa96c9361147d3063b5453d7a780731782bb6f1
Nikolay <nasilaev@edu.hse.ru>
2025-05-02 00:57:08 +0100

    upd

--- open_universe/networks/universe/__init__.py
+++ open_universe/networks/universe/__init__.py
@@ -202,7 +202,7 @@ from .mdn import MixtureDensityNetworkLoss
 # from .condition_01May_512 import ConditionerNetwork 
 from .condition_01May_512_2x import ConditionerNetwork 
 from .m_universe_gan_NS2_adj4 import UniverseGAN
-from .m_universe_gan_NS2_adj4_lgfix import UniverseGAN
+# from .m_universe_gan_NS2_adj4_lgfix import UniverseGAN
 from .textencoder_plbert_op_fix import TextEncoder
 from .score import ScoreNetwork
 
