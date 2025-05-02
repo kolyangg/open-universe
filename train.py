222b1b8a38bd8a80f656c01833a6e40d74a5ceeb
Nikolay <nasilaev@edu.hse.ru>
2025-05-02 21:07:39 +0100

    upd

--- train.py
+++ train.py
@@ -129,7 +129,7 @@ def main(cfg):
 
     wandb_logger = pl_loggers.WandbLogger(
         # project="universe_4s",
-        roject="univ_4gpu",
+        project="univ_4gpu",
         name=exp_name,
         id=wandb_id,                       # None ⇒ start new run
         resume="allow" if wandb_id else None,
