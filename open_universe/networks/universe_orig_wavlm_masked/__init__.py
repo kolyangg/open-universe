de2c8a1466a4d06a3abc75f6ba738ca80b717157
Nikolay <nasilaev@edu.hse.ru>
2025-05-05 01:49:49 +0100

    upd

--- open_universe/networks/universe_orig_wavlm_masked/__init__.py
+++ open_universe/networks/universe_orig_wavlm_masked/__init__.py
@@ -6,6 +6,6 @@ from .mdn import MixtureDensityNetworkLoss
 # # DEFAULT
 from .score import ScoreNetwork
 # from .condition_wv4 import ConditionerNetwork
-from .condition_wv4_double_plus import ConditionerNetwork # 3 MAY - TESTING NEW VERSION (GENERIC + SPEAKER FEATURES)
-# from .condition_wv4_single_plus import ConditionerNetwork # 4 MAY - TESTING PLUS VER OF SINGLE OPTION
+# from .condition_wv4_double_plus import ConditionerNetwork # 3 MAY - TESTING NEW VERSION (GENERIC + SPEAKER FEATURES)
+from .condition_wv4_single_plus import ConditionerNetwork # 4 MAY - TESTING PLUS VER OF SINGLE OPTION
 from .universe_gan_NS_wv_loss import UniverseGAN
