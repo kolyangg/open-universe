0e897c592984530780f2f9c3718fa0e4828f0897
Nikolay <nasilaev@edu.hse.ru>
2025-05-10 23:16:53 +0100

    upd

--- open_universe/networks/universe/universe_gan_NS_10May_fix.py
+++ open_universe/networks/universe/universe_gan_NS_10May_fix.py
@@ -24,7 +24,8 @@ from .blocks import PReLU_Conv
 # Import the "Universe" or any base class you use:
 # from .m_universe_NS_adj3_fix import Universe 
 # from .m_universe_NS_adj3_fix_map_orig import Universe 
-from .universe_NS_04May import Universe 
+# from .universe_NS_04May import Universe 
+from .universe_NS_10May_fix import Universe 
 
 log = logging.getLogger(__name__)
 
